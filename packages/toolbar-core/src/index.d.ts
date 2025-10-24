6e638a101f3926ae9646b2434c8ec61510276513
Tony Mathisen <tony2mat@gmail.com>
2025-10-24 12:27:40 +0200

    Create index.d.ts

---
+++ packages/toolbar-core/src/index.d.ts
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export interface ToolbarCoreProps {
+  // reserverte props for senere bruk
+}
+
+export function ToolbarCore(props: ToolbarCoreProps): React.ReactElement;
+
+declare const _default: typeof ToolbarCore;
+export default _default;
