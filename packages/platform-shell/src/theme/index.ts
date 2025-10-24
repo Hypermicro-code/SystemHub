69acd886296af053c93a426b3afdfc328ab36cf8
Tony Mathisen <tony2mat@gmail.com>
2025-10-24 14:16:57 +0200

    Create index.ts

---
+++ packages/platform-shell/src/theme/index.ts
@@ -0,0 +1,8 @@
+// ==== [BLOCK: Theme: Index] BEGIN ====
+import { colors } from "./colors";
+import { typography } from "./typography";
+import { surfaces } from "./surfaces";
+
+export const theme = { colors, typography, surfaces };
+export type Theme = typeof theme;
+// ==== [BLOCK: Theme: Index] END ====
