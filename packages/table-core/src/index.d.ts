88018038987da70a2425ff7dece337637ee79973
Tony Mathisen <tony2mat@gmail.com>
2025-10-24 12:26:24 +0200

    Create index.d.ts

---
+++ packages/table-core/src/index.d.ts
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export interface TableCoreProps {
+  // reserverte props for senere bruk
+}
+
+export function TableCore(props: TableCoreProps): React.ReactElement;
+
+declare const _default: typeof TableCore;
+export default _default;
