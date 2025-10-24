c005a352734fca61166804aa2388b076691419e6
Tony Mathisen <tony2mat@gmail.com>
2025-10-24 14:16:16 +0200

    Create typography.ts

---
+++ packages/platform-shell/src/theme/typography.ts
@@ -0,0 +1,18 @@
+// ==== [BLOCK: Theme: Typography] BEGIN ====
+export const typography = {
+  fontFamily: `ui-sans-serif, -apple-system, Segoe UI, Roboto, "Helvetica Neue", Arial, "Noto Sans", "Apple Color Emoji", "Segoe UI Emoji"`,
+  sizes: {
+    xs: "11px",
+    sm: "12px",
+    base: "14px",
+    lg: "16px",
+    xl: "20px",
+    h2: "24px",
+  },
+  weights: {
+    regular: 400,
+    medium: 500,
+    bold: 600,
+  },
+};
+// ==== [BLOCK: Theme: Typography] END ====
