54cb514536323a4dce8c81abacb799e1115d244f
Tony Mathisen <tony2mat@gmail.com>
2025-10-25 09:05:39 +0200

    Create TableCore.tsx

---
+++ packages/table-core/src/TableCore.tsx
@@ -0,0 +1,19 @@
+// ==== [BLOCK: TableCore Dummy] BEGIN ====
+import React from "react";
+
+export function TableCore() {
+  return (
+    <div
+      style={{
+        flex: 1,
+        background: "#15171A",
+        color: "#AAA",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      (TableCore placeholder)
+    </div>
+  );
+}
+// ==== [BLOCK: TableCore Dummy] END ====
